3a100aa42d42d79ddcf578a3ee925e3dd7b7aae5
Gaurav Das <gaurav.dns2@gmail.com>
2018-01-01 14:19:33 +0530

    Start bash.exe with arguments/environment for integrated terminals.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = Command::new("C:/msys64/usr/bin/bash.exe");
+    cmd.arg("--login")
+        .arg("-i")
+        .env("CHERE_INVOKING", "1")
+        .env("MSYS2_PATH_TYPE", "inherit")
+        .env("PS1", "$ ");
+    cmd.status().expect("could not start bash.exe");
 }
