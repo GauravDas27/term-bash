33eee7218ac362fb5daad417a0038691e5eed868
Gaurav Das <gaurav.dns2@gmail.com>
2018-01-02 12:25:47 +0530

    Minor cleanup.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let mut cmd = Command::new("C:/msys64/usr/bin/bash.exe");
-    cmd.arg("--login")
+    Command::new("C:/msys64/usr/bin/bash.exe")
+        .arg("--login")
         .arg("-i")
         .env("CHERE_INVOKING", "1")
         .env("MSYS2_PATH_TYPE", "inherit")
-        .env("PS1", "$ ");
-    cmd.status().expect("could not start bash.exe");
+        .env("PS1", "$ ")
+        .status()
+        .expect("could not start bash.exe");
 }
