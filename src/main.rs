0665f9a8521b7945e14c8cdeb8645e6cb1a77849
Gaurav Das <gaurav.dns2@gmail.com>
2018-01-04 18:45:16 +0530

    Forward argument to bash.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
-    Command::new("C:/msys64/usr/bin/bash.exe")
-        .arg("--login")
+    let mut cmmd = Command::new("C:/msys64/usr/bin/bash.exe");
+    cmmd.arg("--login")
         .arg("-i")
         .env("CHERE_INVOKING", "1")
         .env("MSYS2_PATH_TYPE", "inherit")
-        .env("PS1", "$ ")
-        .status()
-        .expect("could not start bash.exe");
+        .env("PS1", "$ ");
+
+
+    let mut argv = env::args();
+    argv.next();
+    loop {
+        match argv.next() {
+            None => break,
+            Some(x) => cmmd.arg(x),
+        };
+    }
+
+    cmmd.status().expect("could not start bash.exe");
 }
